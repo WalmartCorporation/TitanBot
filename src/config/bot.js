751528ec972606bb948fba7af0cf84bdda2e2f50
WalmartCorporation <redcoatcanadien@gmail.com>
2026-04-08 21:09:53 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Keeping Roblox and Discord together!",
+        name: "Linking Roblox and Discord into one seamless experience!",
         // Activity type number (0 = Playing).
         type: 4, 
       },
